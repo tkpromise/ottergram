c20720f38f24542c4e5b3a3a52664b8ec424f908
tkpromise <tkpromise@foxmail.com>
2018-03-25 16:31:33 +0800

    ok

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 var DETAIL_IMAGE_SELECTOR = '[data-image-role="target"]';
 var DETAIL_TITLE_SELECTOR = '[data-image-role="title"]';
+var DETAIL_FRAME_SELECTOR = '[data-image-role="frame"]';
 var THUMBNAIL_LINK_SELECTOR = '[data-image-role="trigger"]';
+var HIDDEN_DETAIL_CLASS = 'hidden-detail';
+var TINY_EFFECT_CLASS = 'is-tity';
+var ESC_KEY = 27;
 
 function setDetails(imageUrl, titleText) {
 	'use strict';
@@ -9,6 +13,31 @@ function setDetails(imageUrl, titleText) {
 	
 	var detailTitle = document.querySelector(DETAIL_TITLE_SELECTOR);
 	detailTitle.textContent = titleText;
+}
+
+function hiddeDetails() {
+	'use strict';
+	document.body.classList.add(HIDDEN_DETAIL_CLASS);
+}
+function showDetails() {
+	'use strict';
+	var frame = document.querySelector(DETAIL_FRAME_SELECTOR);
+	document.body.classList.remove(HIDDEN_DETAIL_CLASS);
+	frame.classList.add(TINY_EFFECT_CLASS);
+	setTimeout(function() {
+		frame.classList.remove(TINY_EFFECT_CLASS);
+	}, 50);
+}
+
+function addKeyPressHandler() {
+	'use strict';
+	document.body.addEventListener('keyup', function(event) {
+		event.preventDefault();
+		console.log(event.keyCode);
+		if (event.keyCode === ESC_KEY) {
+			hiddeDetails();
+		};
+	});
 }
 
 function imageFromThumb(thumbnail) {
@@ -28,6 +57,7 @@ function addThumbClickHandler(thumb) {
 	thumb.addEventListener('click', function(event) {
 		event.preventDefault();
 		setDetailsFromThumb(thumb);
+		showDetails();
 	});
 }
 function getThumbnailsArray() {
@@ -40,5 +70,6 @@ function initializeEvents() {
 	'use strict';
 	var thumbnails = getThumbnailsArray();
 	thumbnails.forEach(addThumbClickHandler);
+	addKeyPressHandler();
 }
 initializeEvents();
